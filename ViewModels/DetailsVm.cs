529851706aeaa8e11d0810f8cb8d971bb174078d
cajzacaj <cajzacaj@gmail.com>
2019-09-05 14:48:10 +0200

    -

---
+++ ViewModels/DetailsVm.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class DetailsVm
+    {
+        public Hero Hero { get; set; }
+        public Comment NewComment { get; set; }
+    }
+}
